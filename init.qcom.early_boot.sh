24be58941dacd2ec1d9ce69faf522fc88ba345f0
Your Name <you@example.com>
2017-02-15 17:26:09 +1100

    clear up early_boot script and force density to 320

--- init.qcom.early_boot.sh
+++ init.qcom.early_boot.sh
@@ -45,29 +45,10 @@ else
     soc_hwver=`cat /sys/devices/system/soc/soc0/platform_version` 2> /dev/null
 fi
 
-if [ -f /sys/class/graphics/fb0/virtual_size ]; then
-    res=`cat /sys/class/graphics/fb0/virtual_size` 2> /dev/null
-    fb_width=${res%,*}
-fi
 
 log -t BOOT -p i "MSM target '$1', SoC '$soc_hwplatform', HwID '$soc_hwid', SoC ver '$soc_hwver'"
 
-function set_density_by_fb() {
-    #put default density based on width
-    if [ -z $fb_width ]; then
         setprop ro.sf.lcd_density 320
-    else
-        if [ $fb_width -ge 1080 ]; then
-           setprop ro.sf.lcd_density 480
-        elif [ $fb_width -ge 720 ]; then
-           setprop ro.sf.lcd_density 320 #for 720X1280 resolution
-        elif [ $fb_width -ge 480 ]; then
-            setprop ro.sf.lcd_density 240 #for 480X854 QRD resolution
-        else
-            setprop ro.sf.lcd_density 160
-        fi
-    fi
-}
 target=`getprop ro.board.platform`
 case "$target" in
     "msm8937" | "msm8940")
@@ -85,11 +66,6 @@ case "$target" in
         esac
         ;;
 esac
-#set default lcd density
-#Since lcd density has read only
-#property, it will not overwrite previous set
-#property if any target is setting forcefully.
-set_density_by_fb
 
 # Setup display nodes & permissions
 # HDMI can be fb1 or fb2
@@ -155,17 +131,6 @@ then
                 setprop debug.gralloc.gfx_ubwc_disable 0
             esac
     done
-fi
-
-file=/sys/class/graphics/fb0
-if [ -d "$file" ]
-then
-        set_perms $file/idle_time system.graphics 0664
-        set_perms $file/dynamic_fps system.graphics 0664
-        set_perms $file/dyn_pu system.graphics 0664
-        set_perms $file/modes system.graphics 0664
-        set_perms $file/mode system.graphics 0664
-        set_perms $file/msm_cmd_autorefresh_en system.graphics 0664
 fi
 
 boot_reason=`cat /proc/sys/kernel/boot_reason`
